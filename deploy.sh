53162e354b51d8061e0f114488cb8467f4d2aa71
Larry Boltovskoi <larry.bolt@gmail.com>
2019-01-15 22:02:05 +0100

    better way to pull submodule changes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR
-git submodule update --recursive --remote
+git pull --recurse-submodules
 docker-compose up -d --build
